77b68b685dd9f69f2ccc569d9389c7ea5dd107f5
taofenghua <taofenghua@bytedance.com>
2020-11-26 20:47:16 +0800

    init

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -60,6 +60,9 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+// 添加 less 解析规则
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -94,7 +97,7 @@ module.exports = function (webpackEnv) {
   const shouldUseReactRefresh = env.raw.FAST_REFRESH;
 
   // common function to get style loaders
-  const getStyleLoaders = (cssOptions, preProcessor) => {
+  const getStyleLoaders = (cssOptions, preProcessor, preProcessorOptions = {}) => {
     const loaders = [
       isEnvDevelopment && require.resolve('style-loader'),
       isEnvProduction && {
@@ -148,6 +151,7 @@ module.exports = function (webpackEnv) {
           loader: require.resolve(preProcessor),
           options: {
             sourceMap: true,
+            ...preProcessorOptions,
           },
         }
       );
@@ -522,6 +526,42 @@ module.exports = function (webpackEnv) {
                 },
                 'sass-loader'
               ),
+            },
+            // Less 解析配置
+            {
+              test: lessRegex,
+              exclude: lessModuleRegex,
+              use: getStyleLoaders(
+                  {
+                      importLoaders: 2,
+                      sourceMap: isEnvProduction && shouldUseSourceMap,
+                  },
+                  'less-loader',
+                  {
+                    lessOptions: {
+                      javascriptEnabled: true,
+                    }
+                  }
+              ),
+              sideEffects: true,
+            },
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders(
+                  {
+                      importLoaders: 2,
+                      sourceMap: isEnvProduction && shouldUseSourceMap,
+                      modules: {
+                        getLocalIdent: getCSSModuleLocalIdent,
+                      },
+                  },
+                  'less-loader',
+                  {
+                    lessOptions: {
+                      javascriptEnabled: true,
+                    }
+                  }
+              )
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
--- src/DragSelector/index.jsx
+++ src/DragSelector/index.jsx
@@ -1,45 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { Checkbox, Row, Col } from "antd"
-import './index.css'
+import styles from './index.module.less'
+import './index.less'
 
 const data = [
   {
     title: "标题1",
+    id: '1',
     description:
-      "Checkbox.Group 内嵌 Checkbox 并与 Grid 组件一起使用，可以实现灵活的布局。",
+      "Checkbox.Group 内嵌 Checkbox 并与 Grid 组件一起使用，可以实现灵活的布局。组件一起使用，可以实现灵活的布局。组件一起使用，可以实现灵活的布局。组件一起使用，可以实现灵活的布局。",
   },
   {
     title: "标题2",
+    id: '2',
     description: "联动 checkbox。",
   },
   {
     title: "标题3",
+    id: '3',
     description: "在实现全选效果时，你可能会用到 indeterminate 属性。",
   },
   {
     title: "标题4",
+    id: '4',
     description: "checkbox 一般用于状态标记，需要和提交操作配合。",
   },
   {
     title: "标题5",
+    id: '5',
     description: "在一组可选项中进行多项选择时；",
   },
 ];
 
 export default function DragSelector() {
+  
+  const [isOnDrag, setIsOnDrag] = useState(false)
+  const [checked, setChecked] = useState([])
+  const [tempChecked, setTempChecked] = useState({})
 
   const onChange = (val) => {
     console.log(val)
+    setChecked([...checked, val])
   }
+
+  function onMouseDown(e) {
+    e.stopPropagation()
+    setIsOnDrag(true)
+    setTempChecked({})
+  }
+
+  function onMouseUp (e) {
+    e.stopPropagation()
+    setIsOnDrag(false)
+    console.log(tempChecked)
+  }
+
+  function handleDragSelected(id) {
+    setTempChecked({
+      ...tempChecked,
+      [id]: true,
+    })
+  }
+
+
+
   return (
-    <Checkbox.Group style={{ width: "100%" }} onChange={onChange}>
+    <Checkbox.Group
+      className={styles.dragSelector} style={{ width: "100%" }}
+      onChange={onChange}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+      value={checked}
+    >
       <Row>
-        {data.map((item, index) => {
+        {data.map(item => {
           return (
-            <Col span={24}>
-              <Checkbox value={index}>
-                <p className='title'>{item.title}</p>
-                <p className='description'>{item.description}</p>
+            <Col
+              key={item.id}
+              span={24}
+              onMouseMove={e => {
+                e.stopPropagation()
+                if (isOnDrag) {
+                  handleDragSelected(item.id)
+                }
+              }}
+            >
+              <Checkbox 
+                className={styles.checkbox}
+                value={item.id}
+              >
+                <p className={styles.title}>{item.title}</p>
+                <p className={styles.description}>{item.description}</p>
               </Checkbox>
             </Col>
           )
